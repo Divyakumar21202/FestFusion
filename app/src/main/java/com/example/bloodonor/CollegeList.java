d37c7ca7ed415fc86a7f13080cda0a5b3c49e4b4
= <b122079@iiit-bh.ac.in>
2023-10-06 21:23:29 +0530

    Our Initialisation of Repository

---
+++ app/src/main/java/com/example/bloodonor/CollegeList.java
@@ -0,0 +1,14 @@
+package com.example.bloodonor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollegeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_college_list);
+    }
+}
